0e3487f6b63fd858ec56c7de58ddaffc3f8e4bd8
DzenVm <prosumerent@gmail.com>
2025-05-04 19:35:59 +0000

    2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,15 @@
-const twTheme = require('./tailwind-plugin/tw-theme');
-const twGrid = require('./tailwind-plugin/tw-bs-grid');
-
 module.exports = {
-  content: [
-    './layouts/**/*.{html,js}',
-    './content/**/*.{md,html}',
-    './themes/**/*.{html,js}',
-  ],
-  safelist: [
-    '!bg-light',
-    '!rounded-lg',
-    '!ml-0',
-    '!pt-0',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        light: '#f9fafb',
-        dark: '#111827',
-      },
+    content: [
+      './layouts/**/*.{html,js}',
+      './content/**/*.{md,html}',
+      './themes/**/*.{html,js}',
+    ],
+    theme: {
+      extend: {},
     },
-  },
-  plugins: [
-    twTheme,
-    twGrid,
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-  ],
-};
+    plugins: [
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/typography'),
+    ],
+  }
+  
